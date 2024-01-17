12cce5221906e5e6ed53ae0231c29efd3850361c
Diwash Rai <raidiwash30@gmail.com>
2024-01-17 22:23:36 +0545

    dashboard

--- lib/features/dashboard/view/dashboard_view.dart
+++ lib/features/dashboard/view/dashboard_view.dart
@@ -1,3 +1,4 @@
+import 'package:anime_quiz/common/widgets/k_chip.dart';
 import 'package:flutter/material.dart';
 
 class DashBoardView extends StatelessWidget {
@@ -6,9 +7,14 @@ class DashBoardView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Dashboard"),
+      appBar: AppBar(
+        title: const Text("Dashboard"),
       ),
+      body: Column(children: [
+        Row(
+          children: [Text("Ram bahadur"), kChip(child: Text("Level 10"))],
+        )
+      ]),
     );
   }
 }
